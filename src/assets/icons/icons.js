a54a996b9c404de428bf4be9bff86c2326bdd754
BuddhadebKoner <jeetkoner36@gmail.com>
2024-06-26 21:55:51 +0530

    image efficient

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,11 +1,9 @@
 import Donate from './donate.png'
 import fb from './fb.png';
-import heroBanner from './hero-banner.webp';
 import home from './home.png';
 import link from './Link.png';
 import linkedin from './linkedin.png';
 import mail from './mail.png';
-import profile from './profile.jpg';
 import threeDot from './threedot.png';
 import x from './x.png';
 import github from './github.png';
@@ -14,6 +12,9 @@ import Like from './like.png'
 import Close from './Close.png'
 import more from './more.png'
 import GpayQR from './GPayQR.svg'
+
+const heroBanner = 'https://i.ibb.co/9VHf0k3/hero-banner.webp'
+const profile = 'https://i.ibb.co/56hzHJn/profile.jpg'
 
 const assets = {
     Donate,
